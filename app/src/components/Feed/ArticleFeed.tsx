3b923e3d4a1c59ab6d6c7da66b8bc675d9eb7e0f
Optifun <frdima19@gmail.com>
2022-10-22 00:23:05 +0700

    Update ArticleFeed: show articles and errors

--- app/src/components/Feed/ArticleFeed.tsx
+++ app/src/components/Feed/ArticleFeed.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { fetchArticles } from "../../store/articleSlice";
+import { ArticleList } from "./ArticleList";
 
-const ArticleFeed: React.FC = () => {
-    return (
-        <div >
 
-        </div>
-    );
+export const ArticleFeed: React.FC = () => {
+  const { list: articles, errors, loading } = useAppSelector(state => state.articles);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    (async () => {
+      dispatch(fetchArticles())
+    })()
+  }, [])
+
+  return (
+    <div>
+      <body>
+      {loading && <h2>Loading...</h2>}
+      {!loading && articles.length == 0 && <h2>There is no articles found</h2>}
+      {!loading && articles.length && <ArticleList items={articles}/>}
+      <div>
+        {errors.map(e => <span>{e}</span>)}
+      </div>
+      </body>
+
+    </div>
+  );
 }
 
 export default ArticleFeed;
